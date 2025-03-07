19e68bde7c60bc96cae82475f88194ed107ae976
MejriSalmaa <salma.mejri@esen.tn>
2025-03-08 00:35:47 +0100

    Save changes before pulling Integration branch

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,46 +1,19 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  Name: { type: String },
+const usersSchema = new mongoose.Schema({
+  Name: String,
   Identifiant: { type: String, unique: true },
-  Email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: {
-      validator: function(value) {
-        return value.toLowerCase().endsWith('@esprit.tn');
-      },
-      message: 'L\'email doit se terminer par "@esprit.tn"'
-    }
-  },
-  //Password: { type: String, required: true },
-  Password: { 
-    type: String,
-    required: function() {
-      // Requis seulement si pas d'authentification Google
-      return !this.googleId; 
-    }
-  },
-  googleId: { type: String, unique: true, sparse: true }, // Ajouter ce champ
-  Classe: { type: String },
+  Email: String,
+  Password: String,
+  Classe: String,
   Role: {
     type: [String],
     enum: ['student', 'admin', 'psychologist', 'teacher'],
     required: true
   }, 
-  PhoneNumber: { type: String },
-  imageUrl: { type: String },
-  verified: { type: Boolean, default: false },
-  otp: { type: String },
-  otpExpires: { type: Date },
-  loginAttempts: { type: Number, default: 0 },
-  TwoFactorEnabled: { type: Boolean, default: false },
-  twoFactorSecret: { type: String },
-}, { 
-  collection: 'users',
-  timestamps: true,
-  strict: false
-});
 
-module.exports = mongoose.models.User || mongoose.model('User', userSchema);
+  PhoneNumber: String,
+  imageUrl: String
+}, { timestamps: true });
+
+module.exports =  mongoose.model('Users', usersSchema);
