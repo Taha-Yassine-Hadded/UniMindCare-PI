3a0f59444b5b1468df6bd3e3986695d7079b2663
baha-eddine-dridi <149486357+baha-eddine-dridi@users.noreply.github.com>
2025-04-17 14:01:36 +0100

    Renamed Users.js to Models/Users.js

--- backend/Models/Users.js
+++ backend/Models/Users.js
@@ -1,19 +1,49 @@
 const mongoose = require('mongoose');
 
-const usersSchema = new mongoose.Schema({
-  Name: String,
+const userSchema = new mongoose.Schema({
+  Name: { type: String },
   Identifiant: { type: String, unique: true },
-  Email: String,
-  Password: String,
-  Classe: String,
+  Email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function(value) {
+        return value.toLowerCase().endsWith('@esprit.tn');
+      },
+      message: 'L\'email doit se terminer par "@esprit.tn"'
+    }
+  },
+  Password: { 
+    type: String,
+    required: function() {
+      return !this.googleId; 
+    }
+  },
+  googleId: { type: String, unique: true, sparse: true },
+  Classe: { type: String },
   Role: {
     type: [String],
-    enum: ['student', 'admin', 'psychologist', 'teacher'],
+    enum: ['student', 'admin', 'psychiatre', 'teacher'],
     required: true
   }, 
+  PhoneNumber: { type: String },
+  imageUrl: { type: String },
+  verified: { type: Boolean, default: false },
+  enabled: { type: Boolean, default: false },
+  otp: { type: String },
+  otpExpires: { type: Date },
+  loginAttempts: { type: Number, default: 0 },
+  twoFactorSecret: String,
+  twoFactorSecretTemp: String,
+  twoFactorEnabled: { type: Boolean, default: false },
+  // Nouveau champ pour les enseignants
+  enableExitRequestSorting: { type: Boolean, default: false }, // Ajouté  // champ ajouté pour la vérification de l'email
+}, { 
+  collection: 'users',
+  timestamps: true,
+  strict: false
+});
 
-  PhoneNumber: String,
-  imageUrl: String
-}, { timestamps: true });
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
 
-module.exports =  mongoose.model('Users', usersSchema);
