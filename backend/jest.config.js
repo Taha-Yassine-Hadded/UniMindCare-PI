a9adb969214cfd3d09b21b2fe6c93626eb914863
MejriSalmaa <salma.mejri@esen.tn>
2025-05-04 16:44:49 +0100

    jest.config.js

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: "node",
+    moduleDirectories: ["node_modules", "Models", "routes"],
+    moduleFileExtensions: ["js", "json"],
+    coveragePathIgnorePatterns: ["/test/"],
+    testPathIgnorePatterns: ['/node_modules/', 'evalution.js'],
+
+  };
