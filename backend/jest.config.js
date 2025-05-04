37949c0ef58eafd9ad53d25219b49af2097cd7ab
MejriSalmaa <salma.mejri@esen.tn>
2025-05-04 19:55:55 +0100

    fix:package json jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
     moduleDirectories: ["node_modules", "Models", "routes"],
     moduleFileExtensions: ["js", "json"],
     coveragePathIgnorePatterns: ["/test/"],
-    };
+    reporters: [
+      "default",
+      ["jest-junit", {
+        outputDirectory: ".",
+        outputName: "junit.xml"
+      }]
+    ]
+  };
